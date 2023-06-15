771f95baee39a5b31f59711061900b131092c59b
zjb0807 <zjb0807@qq.com>
2023-06-15 14:03:16 +0800

    upload log with network name

--- rewards/upload_log.ts
+++ rewards/upload_log.ts
@@ -1,14 +1,16 @@
 import { createFile } from './lib/aws_utils';
 import * as fs from 'fs';
+import { ethers } from 'hardhat';
 
 const main = async () => {
+    const network = await ethers.provider.getNetwork();
     const stdout = process.env.LOG_FILE as string;
     const stdoutContent = fs.readFileSync(stdout).toString();
-    await createFile(`logs/goerli_${stdout}`, stdoutContent);
+    await createFile(`logs/${network.name}_${stdout}`, stdoutContent);
 
     const stderr = process.env.ERROR_LOG_FILE as string;
     const stderrContent = fs.readFileSync(stderr).toString();
-    await createFile(`logs/goerli_${stderr}`, stderrContent);
+    await createFile(`logs/${network.name}_goerli_${stderr}`, stderrContent);
 }
 
 main().then(() => {
