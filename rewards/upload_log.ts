4b1f2c9988545d5dbe0e8b142c539e39618f103a
zjb0807 <zjb0807@qq.com>
2023-06-16 11:21:59 +0800

    fix log file name

--- rewards/upload_log.ts
+++ rewards/upload_log.ts
@@ -10,7 +10,7 @@ const main = async () => {
 
     const stderr = process.env.ERROR_LOG_FILE as string;
     const stderrContent = fs.readFileSync(stderr).toString();
-    await createFile(`logs/${network.name}_goerli_${stderr}`, stderrContent);
+    await createFile(`logs/${network.name}_${stderr}`, stderrContent);
 }
 
 main().then(() => {
